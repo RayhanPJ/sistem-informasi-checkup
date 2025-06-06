45d5e40ee90a5b5e1da71925ae5f9641557a4362
rayhan-putra <rayhanputra100@gmail.com>
2025-06-06 15:12:25 +0700

    push project medical checkup

---
+++ database/migrations/2025_06_06_015444_create_tbl_history_checkup.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tbl_history_checkup', function (Blueprint $table) {
+            $table->id();
+            $table->string('id_mcu')->unique();
+            $table->string('nama_pasien');
+            $table->string('tempat_lahir');
+            $table->date('tanggal_lahir');
+            $table->string('alamat');
+            $table->string('jenis_kelamin');
+            $table->string('pekerjaan');
+            $table->string('nik');
+            $table->string('no_hp');
+            $table->date('tanggal_mcu');
+            $table->string('status_mcu');
+            $table->string('link_hasil_mcu');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tbl_history_checkup');
+    }
+};
